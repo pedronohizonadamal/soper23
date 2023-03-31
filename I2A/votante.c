70a68a55b980513c66ff8090edf373a996a9dcb8
Deneb999 <31244136+Deneb999@users.noreply.github.com>
2023-03-31 13:37:47 +0200

    FINAL

--- I2A/votante.c
+++ I2A/votante.c
@@ -9,7 +9,6 @@ void close_semaphores(){
 
 void safe_exit(int EXIT_TYPE) {
     close_semaphores();
-    printf("%d safe exit\n",getpid());
     exit(EXIT_TYPE);
 }
 
@@ -23,9 +22,6 @@ void sigusr2_handler(int sig){
 
 void sigterm_handler(int sig){ 
     Finalizar_ejecucion = 1;
-    if(Allow_term){
-        safe_exit(EXIT_SUCCESS);
-    }
 }
 
 int rand_bool() {
@@ -48,11 +44,11 @@ void votar() {
         safe_exit(EXIT_FAILURE);
     }
     while (fgets(line, LINE_BUFFER, fp) != NULL) {
+        fflush(stdout);
         if (atoi(strtok(line, "|")) == getpid()) {
             fseek(fp,-2,SEEK_CUR);
             vote = cast_vote(); 
             fwrite(&vote, sizeof(char), 1, fp);
-            fseek(fp, 1, SEEK_CUR);
             break;
         }
     }
@@ -82,7 +78,7 @@ void init_semaphores(){
 }
 
 int main(int argc, char **argv){
-    sigset_t sig1, sig2;
+    sigset_t sigset;
     struct sigaction usr1, usr2, term;
     
 
@@ -128,130 +124,109 @@ int main(int argc, char **argv){
         safe_exit(EXIT_FAILURE);
     }
     
-    sigfillset(&sig1);
-    sigdelset(&sig1, SIGUSR1);
-    sigdelset(&sig1, SIGTERM);
-
-    sigfillset(&sig2);
-    sigdelset(&sig2, SIGUSR2);
-    sigdelset(&sig2, SIGTERM);
+    sigfillset(&sigset);
+    sigdelset(&sigset, SIGUSR1);
+    sigdelset(&sigset, SIGTERM);
     usr1_arrived = 0;
     usr2_arrived = 0;
     /*Avisar de que el votante está listo*/
     sem_post(n_votantes);
     /*Wait for USR1. If USR1 has arrived already, ignore this part*/
     if(usr1_arrived == 0){
-        sigsuspend(&sig1);
+        sigsuspend(&sigset);
     }
-        usr1_arrived = 0;
-    start_round(&sig1, &sig2);
+    start_round();
 }
 
-void start_round(sigset_t *usr1_sig, sigset_t *usr2_sig){
+void start_round(){
+    sigset_t sigset;
     int i, voting_finished, n_votes, vote_balance;
+    int im_candidate;
     char buffer[LINE_BUFFER];
-    struct timespec waiting_time = WAITING_TIME, waiting_round = WAITING_ROUND, waiting_aux = WAITING_AUX;
+    struct timespec waiting_time = WAITING_TIME;
     char read_vote[2];
     char results[RESULTS_SIZE];
     FILE *f;
     
-    while(!Finalizar_ejecucion){
-        usr1_arrived = 0;
+    /*Luchar por ser el proceso candidato*/
+    if (!sem_trywait(candidate)){
+        im_candidate = 1;
+        /*Wait for all voters*/
+        for(i = 0; i<processes-1; i++){
+            sem_wait(n_votantes);
+        }
+        /*Correct candidate sempahore*/
+        sem_post(candidate);
+        /*Send USR2*/
+        kill(-1*getpgid(getpid()),SIGUSR2);
+        voting_finished = 0;
+        while(voting_finished == 0){
+            /*Comprobar fichero*/
+            nanosleep(&waiting_time,NULL);
+            sem_wait(file_mutex);
+            if(!(f = fopen(REGISTER, "r"))){
+                printf("Critical error opening file");
+                safe_exit(EXIT_FAILURE);
+            }
+            strcpy(results, "CANDIDATE ");
+            sprintf(buffer, "%d => [ ", getpid());
+            strcat(results, buffer);
+            n_votes = 0;
+            vote_balance=0;
+            while (fgets(buffer, LINE_BUFFER, f) != NULL) {
+                strtok(buffer, "|");
+                strcpy(read_vote,strtok(NULL, "") );
+                if (read_vote[0] == 'Y') {
+                    sprintf(buffer, "%c ", read_vote[0]);
+                    strcat(results, buffer);
+                    n_votes+=1;
+                    vote_balance+=1;
+                } else if (read_vote[0] == 'N'){
+                    sprintf(buffer, "%c ", read_vote[0]);
+                    strcat(results, buffer);
+                    n_votes+=1;
+                    vote_balance-=1;
+                }
+            }
+            /*Si todos han votado*/
+            if(n_votes == (processes-1)){
+                strcat(results, "] => ");
+                if (vote_balance > 0) strcat(results, "Accepted");
+                else strcat(results, "Rejected");
+                voting_finished = 1;
+                printf("%s\n", results);
+                break;
+            }
+            fclose(f);
+            sem_post(file_mutex);
+        }
+        
+
+    } else {
+        im_candidate = 0;
+
+        sigfillset(&sigset);
+        sigdelset(&sigset, SIGUSR2);
+        sigdelset(&sigset, SIGTERM);
+
+        sem_post(n_votantes);
+        /*Señalizar que estoy listo y esperar a usr2*/
+        if(usr2_arrived == 0){
+            sigsuspend(&sigset);
+        }
         usr2_arrived = 0;
-        /*Luchar por ser el proceso candidato*/
-        ronda++;
-        Allow_term = 0;
-        if (!sem_trywait(candidate)){
-            printf("Proceso %d es candidato ed ronda %d\n",getpid(),ronda);
-            /*Código del candidato*/
-            printf("Gpid es %d\n",getpgid(getpid()));
-            /*Wait for all voters*/
-            for(i = 0; i<processes-1; i++){
-                sem_wait(n_votantes);
-            }
-            /*Send USR2*/
-            nanosleep(&waiting_aux,NULL);   /*OMEGASUS*/
-            kill(-1*getpgid(getpid()),SIGUSR2);
-            voting_finished = 0;
-            while(voting_finished == 0){
-                /*Comprobar fichero*/
-                nanosleep(&waiting_time,NULL);
-                sem_wait(file_mutex);
-                if(!(f = fopen(REGISTER, "r+"))){
-                    printf("Critical error opening file");
-                    safe_exit(EXIT_FAILURE);
-                }
-                strcpy(results, "CANDIDATE ");
-                sprintf(buffer, "%d => [ ", getpid());
-                strcat(results, buffer);
-                n_votes = 0;
-                vote_balance=0;
-                while (fgets(buffer, LINE_BUFFER, f) != NULL) {
-                    strtok(buffer, "|");
-                    strcpy(read_vote,strtok(NULL, "") );
-                    if (read_vote[0] == 'Y') {
-                        sprintf(buffer, "%c ", read_vote[0]);
-                        strcat(results, buffer);
-                        n_votes+=1;
-                        vote_balance+=1;
-                    } else if (read_vote[0] == 'N'){
-                        sprintf(buffer, "%c ", read_vote[0]);
-                        strcat(results, buffer);
-                        n_votes+=1;
-                        vote_balance-=1;
-                    }
-                }
-                /*Si todos han votado*/
-                if(n_votes == (processes-1)){
-                    strcat(results, "] => ");
-                    if (vote_balance > 0) strcat(results, "Accepted");
-                    else strcat(results, "Rejected");
-                    voting_finished = 1;
-                    printf("%s\n", results);
-                    strcpy(read_vote, " ");
-                    fseek(f, 0, SEEK_SET);
-                    while (fgets(buffer, LINE_BUFFER, f) != NULL) {
-                        fseek(f, -2, SEEK_CUR);
-                        fwrite(read_vote, sizeof(char), 1, f);
-                        fseek(f, 1, SEEK_CUR);
-                    }
-                    printf("Round %d finished\n",ronda);
-                    Allow_term = 1;
-                    nanosleep(&waiting_round,NULL);
-                    /*Correct candidate sempahore*/
-                    sem_post(candidate);
-                    kill(-1*getpgid(getpid()),SIGUSR1);
-                }
-                fclose(f);
-                sem_post(file_mutex);
-                
-            }
+        votar();
 
+        if(usr1_arrived == 0){
+            sigsuspend(&sigset);
         }
-
-        else {
-
-            /*Código del votante*/
-            
-            sem_post(n_votantes);
-
-            printf("Process %d esperar Usr2 en ronda %d\n",getpid(),ronda);
-            /*Señalizar que estoy listo y esperar a usr2*/
-
-            if(usr2_arrived == 0){
-                sigsuspend(usr2_sig);
-            }
-            usr2_arrived = 0;
-
-            votar();
-            Allow_term = 1;
-            printf("Process %d ya ha votado en %d\n",getpid(),ronda);
-            /*Wait for USR1. If USR1 has arrived already, ignore this part*/
-            if(usr1_arrived == 0){
-                sigsuspend(usr1_sig);
-            }
-            usr1_arrived = 0;
-        }
+        usr1_arrived = 0;
     }
-    safe_exit(EXIT_SUCCESS);
+    
+    /*Esperar a todos los votantes*/
+
+    while(1) {
+        if (Finalizar_ejecucion == 1) safe_exit(EXIT_SUCCESS);
+    }
+    exit(EXIT_SUCCESS);
 }
