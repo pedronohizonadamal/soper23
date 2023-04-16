6d1c1d5c027c173a8b6329e76a520133760763e4
pedronohizonadamal <91074895+pedronohizonadamal@users.noreply.github.com>
2023-04-16 19:14:23 +0200

    Update procesocomprobador.c

--- I3/procesocomprobador.c
+++ I3/procesocomprobador.c
@@ -1,5 +1,6 @@
 #include "monitor.h"
 
+int current_queue = 0;
 
 int main(int argc, char **argv){
     pid_t pid;
@@ -7,13 +8,15 @@ int main(int argc, char **argv){
     int lag;
     int completion_flag = 0;
     long block;
-    Memory *mem = NULL;
+    struct MemoryQueue *mem_queue = NULL;
+    struct Memory current_mem;
     
     if(argc != 2){
         printf("Incorrect arguments. Usage: ./monitor <lag>\n");
         exit(EXIT_FAILURE);
     }
     lag = atoi(argv[1]);
+
     shm_mem = shm_open(SHARED_MEMORY_NAME, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
     if(shm_mem == -1){
         perror("The memory was already created");
@@ -31,15 +34,15 @@ int main(int argc, char **argv){
         exit(EXIT_FAILURE);
     } else {
         // Proceso candidato
-        if (ftruncate(shm_mem, sizeof(Memory)) == -1) {
+        if (ftruncate(shm_mem, sizeof(struct MemoryQueue)) == -1) {
             perror("ftruncate");
             shm_unlink(SHARED_MEMORY_NAME);
             close(shm_mem);
             exit(1);
         }
-        mem = mmap(NULL, sizeof(Memory), PROT_READ | PROT_WRITE, MAP_SHARED, shm_mem, 0);
+        mem_queue = mmap(NULL, sizeof(struct MemoryQueue), PROT_READ | PROT_WRITE, MAP_SHARED, shm_mem, 0);
         close(shm_mem);
-        if (mem == MAP_FAILED) {
+        if (mem_queue == MAP_FAILED) {
             perror("mmap");
             shm_unlink(SHARED_MEMORY_NAME);
             exit(1);
@@ -56,8 +59,13 @@ int main(int argc, char **argv){
                 completion_flag = 1;
             }
 
+            // FALTA ANADIR EL CONTADOR D LA COLA INTERNA (LOCAL)
+            mem_queue[current_queue % QUEUE_SIZE].flag = true;
+            current_mem = mem_queue[current_queue % QUEUE_SIZE];
+            current_queue++;
+
+
             // Insert the block into shared memory
-            mem->block = block;
 
             // Wait
             sleep(lag);
