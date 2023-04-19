6fce3e6620cd38672ba0c354b9ce83cc9f95ced5
Deneb999 <31244136+Deneb999@users.noreply.github.com>
2023-04-19 17:10:38 +0200

    eso

---
+++ i3b/monitor.c
@@ -0,0 +1,205 @@
+#include "monitor.h"
+#include "pow.h"
+
+/* NOTA: LOS SEMÁFOROS DEBEN SER SIN NOMBRE Y ESTAR DENTRO DEL SEGMENTO DE MEMORIA COMPARTIDA*/
+int main(int argc, char **argv){
+    bool comprobador = true;
+    int shm_mem;
+    int lag;
+    int queue_pos = 0;
+    int completion_flag = 0;
+    struct MemoryQueue *mem_queue = NULL;
+    struct Block block;
+    sem_t *mutex, *queue_space, *queue_blocks, *miner_empty, *miner_fill;
+    mqd_t queue;
+    
+    if(argc != 2){
+        printf("Incorrect arguments. Usage: ./monitor <lag>\n");
+        exit(EXIT_FAILURE);
+    }
+    lag = atoi(argv[1]);
+
+    /*Init shared memory*/
+    shm_mem = shm_open(SHARED_MEMORY_NAME, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+    if(shm_mem == -1){
+        /*The memory was created, become monitor and open the file*/
+        comprobador = false;
+        shm_mem = shm_open(SHARED_MEMORY_NAME, O_RDWR, S_IRUSR | S_IWUSR);
+    }
+
+    /*Init semaphores*/
+    if((mutex = sem_open(SEMAPHORE_MUTEX, O_CREAT, S_IRUSR | S_IWUSR ,1))==SEM_FAILED){
+        perror("Semaphore error");
+        close(shm_mem);
+        exit(EXIT_FAILURE);
+    }
+
+    if((queue_space = sem_open(SEMAPHORE_MULTIPLEX_IN, O_CREAT, S_IRUSR | S_IWUSR ,6))==SEM_FAILED){
+        perror("Semaphore error");
+        sem_unlink(SEMAPHORE_MUTEX);
+        close(shm_mem);
+        exit(EXIT_FAILURE);
+    }
+
+    if((queue_blocks = sem_open(SEMAPHORE_MULTIPLEX_OUT, O_CREAT, S_IRUSR | S_IWUSR ,0))==SEM_FAILED){
+        perror("Semaphore error");
+        sem_unlink(SEMAPHORE_MULTIPLEX_IN);
+        sem_unlink(SEMAPHORE_MUTEX);
+        close(shm_mem);
+        exit(EXIT_FAILURE);
+    }
+
+    if((miner_empty = sem_open(MINER_CANDIDATE_QUEUE_IN, O_CREAT, S_IRUSR | S_IWUSR ,7))==SEM_FAILED){
+        perror("Semaphore error");
+        sem_unlink(SEMAPHORE_MULTIPLEX_IN);
+        sem_unlink(SEMAPHORE_MULTIPLEX_OUT);
+        sem_unlink(SEMAPHORE_MUTEX);
+        close(shm_mem);
+        exit(EXIT_FAILURE);
+    }
+    if((miner_fill = sem_open(MINER_CANDIDATE_QUEUE_OUT, O_CREAT, S_IRUSR | S_IWUSR ,0))==SEM_FAILED){
+        perror("Semaphore error");
+        sem_unlink(SEMAPHORE_MULTIPLEX_IN);
+        sem_unlink(SEMAPHORE_MULTIPLEX_OUT);
+        sem_unlink(SEMAPHORE_MUTEX);
+        sem_unlink(MINER_CANDIDATE_QUEUE_IN);
+        close(shm_mem);
+        exit(EXIT_FAILURE);
+    }
+
+    if (ftruncate(shm_mem, sizeof(struct MemoryQueue)) == -1) {
+            perror("ftruncate");
+            shm_unlink(SHARED_MEMORY_NAME);
+            sem_unlink(SEMAPHORE_MUTEX);
+            sem_unlink(SEMAPHORE_MULTIPLEX_IN);
+            sem_unlink(SEMAPHORE_MULTIPLEX_OUT);
+            sem_unlink(MINER_CANDIDATE_QUEUE_IN);
+            sem_unlink(MINER_CANDIDATE_QUEUE_OUT);
+            close(shm_mem);
+            exit(EXIT_FAILURE);
+        }
+    
+    mem_queue = mmap(NULL, sizeof(struct MemoryQueue), PROT_READ | PROT_WRITE, MAP_SHARED, shm_mem, 0);
+    close(shm_mem);
+    if (mem_queue == MAP_FAILED) {
+        perror("mmap");
+        shm_unlink(SHARED_MEMORY_NAME);
+        sem_unlink(SEMAPHORE_MUTEX);
+        sem_unlink(SEMAPHORE_MULTIPLEX_IN);
+        sem_unlink(SEMAPHORE_MULTIPLEX_OUT);
+        sem_unlink(MINER_CANDIDATE_QUEUE_IN);
+        sem_unlink(MINER_CANDIDATE_QUEUE_OUT);
+        exit(EXIT_FAILURE);
+    }
+
+    if(comprobador == false){
+        // Proceso monitor
+        printf("[%d] Printing blocks...\n",(int) getpid());
+        // Loop until we receive the special completion block
+        while (!completion_flag) {
+            //Read block from shared memory
+            sem_wait(queue_blocks);
+            sem_wait(mutex);
+            block = mem_queue->queue[queue_pos%QUEUE_SIZE];
+            sem_post(mutex);
+            sem_post(queue_space);
+            queue_pos++;
+
+            // Check if the received block is the special completion block
+            if(block.end == true){
+                completion_flag = 1;
+            } else {
+                if (block.flag == true){
+                    printf("Solution accepted: %08ld --> %08ld\n",block.target, block.solution);
+                } else {
+                    printf("Solution rejected: %08ld !-> %08ld\n",block.target, block.solution);
+                }
+                // Wait
+                sleep(lag);
+            }
+        }
+    } else {
+        // Proceso comprobador
+        /*init_semaphores(mutex, queue_space, queue_blocks);*/
+        printf("[%d] Checking blocks...\n",(int) getpid());
+
+        /* Message Queue */
+        queue = mq_open (
+        MQ_NAME ,
+        O_CREAT | O_RDONLY , /* This process is going to receive messages . */
+        S_IRUSR | S_IWUSR , /* The user can read and write . */
+        &attributes ) ;
+
+        if ( queue == ( mqd_t ) -1) {
+        fprintf ( stderr , "Error opening the queue \n " ) ;
+        return EXIT_FAILURE ;
+        }
+
+        // Loop until we receive the special completion block
+        while (!completion_flag) {
+            // Receive a block 
+            receive_block(&block, miner_fill, miner_empty, queue);
+
+            // Check if the received block is the special completion block
+            if(block.end == true){
+                completion_flag = 1;
+            } else {
+                //Comprobar si el bloque es correcto. Añadir al bloque una flag para indicarlo.
+                check_block(&block);
+            }
+
+            // Insert the block into shared memory
+            sem_wait(queue_space);
+            sem_wait(mutex);
+            mem_queue->queue[queue_pos%QUEUE_SIZE] = block;
+            sem_post(mutex);
+            sem_post(queue_blocks);
+            queue_pos++;
+
+            // Wait
+            sleep(lag);
+        }
+    }
+    // Free resources and terminate
+        printf("[%d] Finishing\n",(int) getpid());
+        munmap(mem_queue, sizeof(struct MemoryQueue));
+        shm_unlink(SHARED_MEMORY_NAME);
+        sem_unlink(SEMAPHORE_MUTEX);
+        sem_unlink(SEMAPHORE_MULTIPLEX_IN);
+        sem_unlink(SEMAPHORE_MULTIPLEX_OUT);
+        sem_unlink(MINER_CANDIDATE_QUEUE_IN);
+        sem_unlink(MINER_CANDIDATE_QUEUE_OUT);
+        mq_close ( queue );
+        exit(EXIT_SUCCESS);
+}
+
+
+void check_block(struct Block *block){
+    if(pow_hash(block->solution) == block->target){
+        block->flag = true;
+    } else {
+        block->flag = false;
+    }
+    return;
+}
+
+void receive_block(struct Block *block, sem_t *miner_fill, sem_t *miner_empty, mqd_t queue){
+    sem_wait(miner_fill);
+    if ( mq_receive ( queue , ( char *) &block , sizeof (block) , NULL ) == -1) {
+        fprintf ( stderr , "Error receiving message \n " ) ;
+        return;
+        }
+    sem_post(miner_empty);
+    
+    return;
+}
+
+void init_semaphores(sem_t *mutex, sem_t *in, sem_t *out){
+    /*if (!(sem_init(mutex,1,1))){
+        perror("Semaphore error");
+        munmap(mem_queue, sizeof(struct MemoryQueue));
+        sem_unlink(MINER_CANDIDATE_QUEUE_IN);
+        sem_unlink(MINER_CANDIDATE_QUEUE_OUT);
+        exit(EXIT_FAILURE);
+    }*/
+}
---
+++ i3b/monitor.h
@@ -0,0 +1,58 @@
+#ifndef MONITOR_H
+#define MONITOR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h> 
+#include <sys/mman.h>
+#include <unistd.h> 
+#include <sys/wait.h>
+#include <stdbool.h>
+#include <semaphore.h>
+#include <mqueue.h>
+
+#define SHMSZ 27
+#define SHMFILENAME "shmfile.h"
+
+#define IPC_RESULT_ERROR -1
+#define SHARED_MEMORY_NAME "monitor_shmem"
+#define SEMAPHORE_MUTEX "candidate_mutex"
+#define SEMAPHORE_MULTIPLEX_IN "candidate_multiplex_in"
+#define SEMAPHORE_MULTIPLEX_OUT "candidate_multiplex_out"
+#define MINER_CANDIDATE_QUEUE_IN "miner_candidate_multiplex_in"
+#define MINER_CANDIDATE_QUEUE_OUT "miner_candidate_multiplex_out"
+#define COMPLETION_BLOCK "XD"
+#define INITAL_ANSWER 0
+
+#define QUEUE_SIZE 6
+
+#define MQ_NAME "/m_queue"
+
+struct Block{
+    long int target;
+    long int solution;
+    bool flag;
+    bool end;
+};
+
+struct MemoryQueue {
+    struct Block queue[QUEUE_SIZE];
+};
+
+struct mq_attr attributes = {. mq_flags = 0 ,
+    . mq_maxmsg = 7 ,
+    . mq_curmsgs = 0 ,
+    . mq_msgsize = sizeof ( struct Block ) };
+
+void receive_block(struct Block *block, sem_t *miner_fill, sem_t *miner_empty, mqd_t queue);
+void check_block(struct Block *block);
+long get_sol(long target);
+void init_semaphores(sem_t *mutex, sem_t *in, sem_t *out);
+
+#endif
+
+
